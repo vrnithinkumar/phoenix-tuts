aad33ae2041b0285eb396cbeef9fb00fc645acb4
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2024-04-01 15:31:53 +0200

    plung functional

--- lib/phxtest_web/endpoint.ex
+++ lib/phxtest_web/endpoint.ex
@@ -48,5 +48,16 @@ defmodule PhxtestWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug :introspect
   plug PhxtestWeb.Router
+
+  def introspect(conn, _opts) do
+    IO.puts """
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """
+    conn
+  end
+  
 end
