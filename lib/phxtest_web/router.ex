074245739d50d57763fe054a02d22c9ec9353b14
Nithin Vadukkumchery Rajendrakumar <vrnithinkumar@gmail.com>
2024-04-01 15:57:49 +0200

    Module plug

--- lib/phxtest_web/router.ex
+++ lib/phxtest_web/router.ex
@@ -8,6 +8,7 @@ defmodule PhxtestWeb.Router do
     plug :put_root_layout, html: {PhxtestWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug PhxtestWeb.Plugs.Locale, "en"
   end
 
   pipeline :api do
